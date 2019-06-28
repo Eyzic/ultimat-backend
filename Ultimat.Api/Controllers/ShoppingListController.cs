e4e54854d65c3a261883c014a0288d5b9d2ecf53
Gabriel Ingemarsson <tr4zhm4il@gmail.com>
2019-06-28 14:23:42 +0200

    Feat: Add basic shopping-list support (resolve#2)
    Add API endpoints for creating, getting, updating and deleting both
    shopping lists and their items. For now, three dictionaries are used as
    the database until a more permanent solution has been decided upon.

--- Ultimat.Api/Controllers/ShoppingListController.cs
+++ Ultimat.Api/Controllers/ShoppingListController.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Ultimat.Api.Entities;
+using Ultimat.Api.Entities.Requests;
+using Ultimat.Api.Services;
 
 namespace Ultimat.Api.Controllers
 {
@@ -11,5 +14,183 @@ namespace Ultimat.Api.Controllers
     [ApiController]
     public class ShoppingListController : ControllerBase
     {
+        private readonly IShoppingListService _service;
+
+        public ShoppingListController(IShoppingListService service)
+        {
+            _service = service;
+        }
+
+        // List operations
+
+        /// <summary>
+        /// Get a list of all available shopping lists
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public ActionResult<IEnumerable<ShoppingList>> GetAllShoppingLists()
+        {
+            return _service.GetAllLists().ToList();
+        }
+
+        /// <summary>
+        /// Get a specified shopping list
+        /// </summary>
+        /// <param name="listId">The id of the shopping list to fetch</param>
+        /// <returns></returns>
+        [HttpGet("{listId}")]
+        public ActionResult<ShoppingListWithItems> GetShoppingList([FromRoute] Guid listId)
+        {
+            var res = _service.GetList(listId);
+
+            if (res == null)
+                return NotFound();
+
+            return res;
+        }
+
+        /// <summary>
+        /// Create a new shopping list
+        /// </summary>
+        /// <param name="request">The shopping list to create</param>
+        /// <returns></returns>
+        [HttpPost]
+        public ActionResult<ShoppingList> CreateShoppingList([FromBody] ShoppingListRequest request)
+        {
+            if (!ModelState.IsValid) return BadRequest(ModelState);
+
+            var shoppingList = new ShoppingList
+            {
+                Id = Guid.NewGuid(),
+                Name = request.Name
+            };
+
+            return _service.AddList(shoppingList);
+        }
+
+        /// <summary>
+        /// Update a shopping list. This does not modify the items.
+        /// </summary>
+        /// <param name="listId">The id of the list to update</param>
+        /// <param name="request">The new list</param>
+        /// <returns></returns>
+        [HttpPut("{listId}")]
+        public ActionResult<ShoppingList> UpdateShoppingList([FromRoute] Guid listId, [FromBody] ShoppingListRequest request)
+        {
+            if (!ModelState.IsValid) return BadRequest(ModelState);
+
+            var shoppingList = new ShoppingList
+            {
+                Id = listId,
+                Name = request.Name
+            };
+
+            var res = _service.UpdateList(shoppingList);
+
+            if (res == null)
+                return NotFound();
+
+            return res;
+        }
+
+        /// <summary>
+        /// Delete a specified shopping list
+        /// </summary>
+        /// <param name="listId">The list to delete</param>
+        /// <returns></returns>
+        [HttpDelete("{listId}")]
+        public ActionResult DeleteList([FromRoute] Guid listId)
+        {
+            var res = _service.DeleteList(listId);
+
+            if (res) return Ok();
+            else return NotFound();
+        }
+
+
+        // Item operations
+
+        /// <summary>
+        /// Get a specified item of a specified list
+        /// </summary>
+        /// <param name="listId">The id of the list of the item</param>
+        /// <param name="id">The id of the item to get</param>
+        /// <returns></returns>
+        [HttpGet("{listId}/items/{id}")]
+        public ActionResult<ShoppingItem> GetItem([FromRoute] Guid listId, [FromRoute] Guid id)
+        {
+            var res = _service.GetItem(listId, id);
+
+            if (res == null)
+                return NotFound();
+
+            return res;
+        }
+
+        /// <summary>
+        /// Add a new item to specified list
+        /// </summary>
+        /// <param name="listId">The id of the list to add to</param>
+        /// <param name="request">The item to add</param>
+        /// <returns></returns>
+        [HttpPost("{listId}")]
+        public ActionResult<ShoppingItem> CreateItem([FromRoute] Guid listId, [FromBody] ShoppingItemRequest request)
+        {
+            if (!ModelState.IsValid) return BadRequest(ModelState);
+
+            var shoppingItem = new ShoppingItem
+            {
+                Id = Guid.NewGuid(),
+                Description = request.Description,
+                Checked = request.Checked
+            };
+
+            var res = _service.AddItem(listId, shoppingItem);
+
+            if (res == null)
+                return NotFound();
+
+            return res;
+        }
+
+        /// <summary>
+        /// Update a specified item of a specified list
+        /// </summary>
+        /// <param name="listId">The id of the list of the item</param>
+        /// <param name="Id">The id of the item</param>
+        /// <param name="request">The new item</param>
+        /// <returns></returns>
+        [HttpPut("{listId}/items/{id}")]
+        public ActionResult<ShoppingItem> UpdateItem([FromRoute] Guid listId, [FromRoute] Guid Id, [FromBody] ShoppingItemRequest request)
+        {
+            var shoppingItem = new ShoppingItem
+            {
+                Id = Id,
+                Description = request.Description,
+                Checked = request.Checked
+            };
+
+            var res = _service.UpdateItem(listId, shoppingItem);
+
+            if (res == null)
+                return NotFound();
+
+            return res;
+        }
+
+        /// <summary>
+        /// Delete an item of a specified list
+        /// </summary>
+        /// <param name="listId">The id of the list of the item</param>
+        /// <param name="Id">The id of the item to be deleted</param>
+        /// <returns></returns>
+        [HttpDelete("{listId}/items/{id}")]
+        public ActionResult DeleteItem([FromRoute] Guid listId, [FromRoute] Guid Id)
+        {
+            var res = _service.DeleteItem(listId, Id);
+
+            if (res) return Ok();
+            else return NotFound();
+        }
     }
 }
--- Ultimat.Api/Repositories/Interfaces/IShoppingListRepository.cs
+++ Ultimat.Api/Repositories/Interfaces/IShoppingListRepository.cs
@@ -1,6 +1,20 @@
-﻿namespace Ultimat.Api.Repositories
+﻿using System;
+using System.Collections.Generic;
+using Ultimat.Api.Entities;
+
+namespace Ultimat.Api.Repositories
 {
     public interface IShoppingListRepository
     {
+        IEnumerable<ShoppingList> GetAllLists();
+        ShoppingListWithItems GetList(Guid id);
+        ShoppingList SaveList(ShoppingList list);
+        ShoppingList UpdateList(ShoppingList list);
+        Boolean DeleteList(Guid id);
+
+        ShoppingItem GetItem(Guid listId, Guid id);
+        ShoppingItem SaveItem(Guid listId, ShoppingItem item);
+        ShoppingItem UpdateItem(Guid listId, ShoppingItem item);
+        Boolean DeleteItem(Guid listId, Guid id);
     }
 }
